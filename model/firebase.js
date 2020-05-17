d9af954dd609a16a5aab9a7aa432617cdb3f3cb8
scd7896 <scd1212@naver.com>
2020-05-17 14:32:53 +0900

    파이어베이스 설정연결

---
+++ model/firebase.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+dotenv.config()
+const firebase = require('firebase');
+const config = {
+	apiKey: process.env.apiKey,
+	authDomain: process.env.authDomain,
+	databaseURL: process.env.databaseURL,
+	projectId: process.env.projectId,
+	storageBucket: process.env.storageBucket,
+	messagingSenderId: process.env.messagingSenderId,
+	appId: process.env.appId
+}
+const database = firebase.initializeApp(config).database();
+
+
