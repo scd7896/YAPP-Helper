f2dfd8e3888a1068d9e3dddc61e9e99d7ee4b5a6
scd7896 <scd1212@naver.com>
2020-05-27 18:28:56 +0900

    atomic 수직바 추가

---
+++ src/component/atomic/IconWrapper/VerticalBar/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+import './styles.scss'
+const VerticalBar = () => {
+	return (
+		<div className="vertical-bar-style">&nbsp;</div>
+	)
+}
+
+export default VerticalBar;
