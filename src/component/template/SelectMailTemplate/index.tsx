193ad2cf9be44ff6583b00e09e1c894e2842eedd
scd7896 <scd1212@naver.com>
2020-05-28 01:42:10 +0900

    결과 메일 발송 선택 화면 confirm 전 완성

--- src/component/template/SelectMailTemplate/index.tsx
+++ src/component/template/SelectMailTemplate/index.tsx
@@ -3,13 +3,29 @@ import LinkBlock from '../../atomic/LinkBlock'
 import SelectLinkBox from '../../molecules/SelectLinkBox'
 
 import './styles.scss'
+import HeadTitleText from '../../atomic/FontStyle/HeadTitle'
+import BigSubTitle from '../../atomic/FontStyle/Subtitle/Big'
+import SmallSubTitle from '../../atomic/FontStyle/Subtitle/Small'
+import SmallIconWrapper from '../../atomic/IconWrapper/Small'
 const SelectMailTemplate = () => {
 	return (
 		<section className="email-section-wrapper">
-			<section className="email-select-container">
-				<SelectLinkBox to="/email/document/1" title="서류전형"></SelectLinkBox>
-				<SelectLinkBox to="/email/meeting/1" title="면접전형"></SelectLinkBox>
-			</section>
+			<div className="content-width-resize-wrapper">
+				<section className="email-head-container">
+					<div className="email-headtitle-wrapper">
+						<SmallIconWrapper width={26} height={26}></SmallIconWrapper>
+						<div className="for-marginleft-10px">
+							<HeadTitleText>결과메일 발송</HeadTitleText>
+						</div>
+					</div>
+					<SmallSubTitle>발표전형선택</SmallSubTitle>
+					<BigSubTitle>발표할 전형을 선택해주세요</BigSubTitle>
+				</section>
+				<section className="email-select-container">
+					<SelectLinkBox to="/email/document/1" title="서류전형"></SelectLinkBox>
+					<SelectLinkBox to="/email/meeting/1" title="면접전형"></SelectLinkBox>
+				</section>
+			</div>
 		</section>
 	)
 }
