c7b21b8e725fb12c285d2b139385228526428785
scd7896 <scd1212@naver.com>
2020-05-30 15:43:55 +0900

    input guide title 적용

--- src/component/atomic/FontStyle/InnerInputGuide/Title/index.tsx
+++ src/component/atomic/FontStyle/InnerInputGuide/Title/index.tsx
@@ -1,1 +1,12 @@
 import * as React from 'react'
+
+import './styles.scss'
+const InnerInputGuideTitle = ({ children }: FontStyle) => {
+	return (
+		<p className="inner-inputtitle-style">
+			{children}
+		</p>
+	)
+}
+
+export default InnerInputGuideTitle
--- src/component/organisms/EmailGrade/First/index.tsx
+++ src/component/organisms/EmailGrade/First/index.tsx
@@ -3,6 +3,8 @@ import FileInput from '../../../atomic/File/Input'
 
 import './styles.scss'
 import InnerHeadStyle from '../../../atomic/FontStyle/InnerHeadStyle'
+import InnerInputGuideTitle from '../../../atomic/FontStyle/InnerInputGuide/Title'
+import InnerInputGuideDescription from '../../../atomic/FontStyle/InnerInputGuide/TitleDescription'
 
 const EmailGradeFirst = () => {
 	return (
@@ -12,6 +14,12 @@ const EmailGradeFirst = () => {
 			</header>
 			<section className="inner-grade-bodysize-wrapper">
 				<FileInput />
+				<section className="inner-grade-input-wrapper">
+					<article className="inner-grade-input-header-wrapper">
+						<InnerInputGuideTitle>엑셀 셀헤더 입력</InnerInputGuideTitle>
+						<InnerInputGuideDescription>해당하는 셀 헤더를 입력하세요</InnerInputGuideDescription>
+					</article>
+				</section>
 			</section>
 		</div>
 	)
