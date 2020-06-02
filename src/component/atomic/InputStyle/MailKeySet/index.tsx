2fd3055c5ac97e67ef5f9dd67ba4035b6a04c340
scd7896 <scd1212@naver.com>
2020-06-02 23:27:18 +0900

    input에서 Dispatch하기 위한 action 함수 export 및 사용

--- src/component/atomic/InputStyle/MailKeySet/index.tsx
+++ src/component/atomic/InputStyle/MailKeySet/index.tsx
@@ -1,8 +1,18 @@
 import * as React from 'react'
-
-const MailKeySetInput = () => {
+import { useDispatch } from 'react-redux'
+import { setExcelKeyValue } from '../../../../store/action/excelKeySetForm'
+interface MailKeySetInputProp {
+	name: string
+	value: string
+}
+const MailKeySetInput = ({ name, value }: MailKeySetInputProp) => {
+	const dispatch = useDispatch();
+	const onChangeEvent = (event: React.ChangeEvent<HTMLInputElement>) => {
+		dispatch(setExcelKeyValue({ key: name, value: event.target.value }))
+	}
+	
 	return(
-		<input></input>
+		<input onChange={onChangeEvent} value={value} />
 	)
 }
 
--- src/store/action/excelKeySetForm.ts
+++ src/store/action/excelKeySetForm.ts
@@ -1,6 +1,6 @@
 import { SET_EXCEL_TEXT } from "./actionTypes"
 
-const setExcelKeyValue = (payload: excelValueChangePayload) => {
+export const setExcelKeyValue = (payload: excelValueChangePayload) => {
 	return {
 		type: SET_EXCEL_TEXT,
 		payload
