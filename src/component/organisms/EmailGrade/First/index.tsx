ce98b32c0aea92b20e3f3e16b25d58fe289bca87
scd7896 <scd1212@naver.com>
2020-06-02 23:27:37 +0900

    rootStore 타입 추가

--- src/component/organisms/EmailGrade/First/index.tsx
+++ src/component/organisms/EmailGrade/First/index.tsx
@@ -5,8 +5,11 @@ import './styles.scss'
 import InnerHeadStyle from '../../../atomic/FontStyle/InnerHeadStyle'
 import InnerInputGuideTitle from '../../../atomic/FontStyle/InnerInputGuide/Title'
 import InnerInputGuideDescription from '../../../atomic/FontStyle/InnerInputGuide/TitleDescription'
+import MailKeySetInput from '../../../atomic/InputStyle/MailKeySet'
+import { useSelector } from 'react-redux'
 
 const EmailGradeFirst = () => {
+	const  excelFormState = useSelector<RootStore>(state => state.excelKeySetForm) as excelKeySetFormState
 	return (
 		<div>
 			<header className="inner-grade-title-wrapper">
@@ -18,6 +21,18 @@ const EmailGradeFirst = () => {
 					<article className="inner-grade-input-header-wrapper">
 						<InnerInputGuideTitle>엑셀 셀헤더 입력</InnerInputGuideTitle>
 						<InnerInputGuideDescription>해당하는 셀 헤더를 입력하세요</InnerInputGuideDescription>
+						<div>
+							<MailKeySetInput name="email" value={excelFormState.email} />
+						</div>
+						<div>
+							<MailKeySetInput name="isPass" value={excelFormState.isPass} />
+						</div>
+						<div>
+							<MailKeySetInput name="name" value={excelFormState.name} />
+						</div>
+						<div>
+							<MailKeySetInput name="meetingTime" value={excelFormState.meetingTime} />
+						</div>
 					</article>
 				</section>
 			</section>
