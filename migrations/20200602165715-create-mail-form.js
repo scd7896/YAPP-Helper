a479072e6bc2f577e53ff3955c9429907b1ce29d
JongminChoe <aaaa961224@gmail.com>
2020-06-07 00:07:47 +0900

    MailForm 마이그레이션 및 모델 생성

---
+++ migrations/20200602165715-create-mail-form.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('MailForms', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING(60)
+      },
+      type: {
+        type: Sequelize.STRING(60)
+      },
+      contents: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('MailForms');
+  }
+};
