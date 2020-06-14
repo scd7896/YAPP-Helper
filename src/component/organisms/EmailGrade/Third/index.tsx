39a1ce8c16fd1ef7d3bd99e376acacc596dbb37e
scd7896 <scd1212@naver.com>
2020-06-14 15:11:54 +0900

    classnames 모듈 추가해서 클래스 이름 중복 문제 해결

--- src/component/organisms/EmailGrade/Third/index.tsx
+++ src/component/organisms/EmailGrade/Third/index.tsx
@@ -1,16 +1,44 @@
 import * as React from 'react'
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { setUserDataByFormRequest } from '../../../../store/action/desire'
+import classNames from 'classnames/bind';
+import styles from './styles.scss'
+
+const cx = classNames.bind(styles)
 
 const EmailGradeThird = () => {
 	const dispatch = useDispatch()
+	const { allList } = useSelector<RootStore>(state => state.desire) as DesireState
 	useEffect(() => {
 		dispatch(setUserDataByFormRequest())
 	}, [])
 	return (
-		<div>
-			3번째 페이지는 이것이다
+		<div className={cx('wrapper')}>
+			<p>3. 셀 분류확인</p>
+			<p>분류완료! 잘 분류되었는지 명단을 확인하세요</p>
+			<table>
+				<thead>
+					<tr>
+						<td>name</td>
+						<td>email</td>
+						<td>isPass</td>
+						<td>meetingTime</td>
+					</tr>
+				</thead>
+				<tbody>
+					{allList.map((user) => {
+						return (
+							<tr>
+								<td>{user.name}</td>
+								<td>{user.email}</td>
+								<td>{user.isPass.toString()}</td>
+								<td>{user.meetingTime ? user.meetingTime : ""}</td>
+							</tr>
+						)
+					})}
+				</tbody>
+			</table>
 		</div>
 	)
 }
