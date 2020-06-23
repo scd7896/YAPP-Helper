093c9d1de18e7f16eb0a8729c0273dda91d35959
JongminChoe <aaaa961224@gmail.com>
2020-06-23 23:10:14 +0900

    MailForm 모델 pass 필드 추가

--- models/mailform.js
+++ models/mailform.js
@@ -3,6 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const MailForm = sequelize.define('MailForm', {
     title: DataTypes.STRING(60),
     type: DataTypes.STRING(60),
+    pass: DataTypes.BOOLEAN,
     contents: DataTypes.TEXT,
     header_image: DataTypes.TEXT,
     map_image: DataTypes.TEXT
--- seeders/20200623113210-mailform-sample.js
+++ seeders/20200623113210-mailform-sample.js
@@ -3,7 +3,8 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
       return queryInterface.bulkInsert('MailForms', [{
         title: 'Lorem ipsum dolor sit amet',
-        type: 'pass1',
+        type: 'meeting',
+        pass: true,
         contents: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in viverra ex, a pulvinar felis. Suspendisse a purus gravida, rutrum sem nec, hendrerit diam. Sed rhoncus venenatis dapibus. Integer in justo ante. In molestie vestibulum urna in efficitur. Mauris non faucibus dolor. Nam vulputate sed dolor id maximus. Integer placerat leo quis felis dictum venenatis. Nulla sed dui non odio euismod consectetur. Mauris placerat iaculis nibh et imperdiet.',
         header_image: 'https://picsum.photos/500/100',
         map_image: 'https://picsum.photos/500/300',
@@ -11,7 +12,8 @@ module.exports = {
         updatedAt: new Date()
       }, {
         title: 'consectetur adipiscing elit',
-        type: 'fail1',
+        type: 'meeting',
+        pass: false,
         contents: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in viverra ex, a pulvinar felis. Suspendisse a purus gravida, rutrum sem nec, hendrerit diam. Sed rhoncus venenatis dapibus. Integer in justo ante. In molestie vestibulum urna in efficitur. Mauris non faucibus dolor. Nam vulputate sed dolor id maximus. Integer placerat leo quis felis dictum venenatis. Nulla sed dui non odio euismod consectetur. Mauris placerat iaculis nibh et imperdiet.',
         header_image: 'https://picsum.photos/500/100',
         map_image: 'https://picsum.photos/500/300',
@@ -19,7 +21,8 @@ module.exports = {
         updatedAt: new Date()
       }, {
         title: 'Duis in viverra ex',
-        type: 'pass2',
+        type: 'document',
+        pass: true,
         contents: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in viverra ex, a pulvinar felis. Suspendisse a purus gravida, rutrum sem nec, hendrerit diam. Sed rhoncus venenatis dapibus. Integer in justo ante. In molestie vestibulum urna in efficitur. Mauris non faucibus dolor. Nam vulputate sed dolor id maximus. Integer placerat leo quis felis dictum venenatis. Nulla sed dui non odio euismod consectetur. Mauris placerat iaculis nibh et imperdiet.',
         header_image: 'https://picsum.photos/500/100',
         map_image: 'https://picsum.photos/500/300',
@@ -27,7 +30,8 @@ module.exports = {
         updatedAt: new Date()
       }, {
         title: 'a pulvinar felis',
-        type: 'fail2',
+        type: 'document',
+        pass: false,
         contents: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in viverra ex, a pulvinar felis. Suspendisse a purus gravida, rutrum sem nec, hendrerit diam. Sed rhoncus venenatis dapibus. Integer in justo ante. In molestie vestibulum urna in efficitur. Mauris non faucibus dolor. Nam vulputate sed dolor id maximus. Integer placerat leo quis felis dictum venenatis. Nulla sed dui non odio euismod consectetur. Mauris placerat iaculis nibh et imperdiet.',
         header_image: 'https://picsum.photos/500/100',
         map_image: 'https://picsum.photos/500/300',
