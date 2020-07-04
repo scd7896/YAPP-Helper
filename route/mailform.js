ab18dceae02f205aa7beefaa339f135733cc9931
JongminChoe <aaaa961224@gmail.com>
2020-07-04 16:35:05 +0900

    코드 포맷팅

--- route/mailform.js
+++ route/mailform.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const multer  = require('multer');
+const multer = require('multer');
 const path = require('path');
 const crypto = require('crypto');
 
@@ -9,9 +9,9 @@ const storage = multer.diskStorage({
     cb(null, 'public');
   },
   filename: function (req, file, cb) {
-		crypto.randomBytes(16, function (err, raw) {
-			cb(err, err ? undefined : raw.toString('hex') + path.extname(file.originalname));
-		});
+    crypto.randomBytes(16, function (err, raw) {
+      cb(err, err ? undefined : raw.toString('hex') + path.extname(file.originalname));
+    });
   }
 });
 const upload = multer({ storage: storage });
@@ -20,40 +20,40 @@ const controller = require('../controller/mailform');
 const { MailForm } = require('../models');
 
 router.route('/')
-	.get(controller.index)
-	.post([
-		upload.fields([{ name: 'header_image' }, { name: 'map_image' }]),
-		controller.store
-	]);
+  .get(controller.index)
+  .post([
+    upload.fields([{ name: 'header_image' }, { name: 'map_image' }]),
+    controller.store
+  ]);
 
 router.route('/type/:type')
-	.get(controller.searchByType);
+  .get(controller.searchByType);
 
 router.route('/:mailform_id')
-	.get(controller.show)
-	.put([
-		upload.fields([{ name: 'header_image' }, { name: 'map_image' }]),
-		controller.update
-	])
-	.delete(controller.destroy);
+  .get(controller.show)
+  .put([
+    upload.fields([{ name: 'header_image' }, { name: 'map_image' }]),
+    controller.update
+  ])
+  .delete(controller.destroy);
 
 router.param('mailform_id', function (req, res, next, id) {
-	const mailform_id = parseInt(id);
-	if (isNaN(mailform_id)){
-		res.sendStatus(400);
-	}
-	else {
-		MailForm.findByPk(mailform_id)
-		.then(mailform => {
-			if (mailform === null) {
-				res.sendStatus(404);
-			}
-			else {
-				req.mailform = mailform;
-				next();
-			}
-		});
-	}
+  const mailformId = parseInt(id);
+  if (isNaN(mailformId)) {
+    res.sendStatus(400);
+  }
+  else {
+    MailForm.findByPk(mailformId)
+      .then(mailform => {
+        if (mailform === null) {
+          res.sendStatus(404);
+        }
+        else {
+          req.mailform = mailform;
+          next();
+        }
+      });
+  }
 });
 
 module.exports = router;
