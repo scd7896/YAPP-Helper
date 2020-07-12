8ea7a6536a61323da1bb9505007c8c0ad7de18d4
JongminChoe <aaaa961224@gmail.com>
2020-07-12 23:57:08 +0900

    MailForm 수정 & 삭제시 불필요한 파일 제거 및 리팩토링

--- controller/mailform.js
+++ controller/mailform.js
@@ -1,70 +1,78 @@
 const { MailForm } = require('../models');
+const { unlink } = require('fs');
 
-const index = (req, res) => {
+const index = (req, res, next) => {
   MailForm
     .scope('orderByType', 'passedFirst')
     .findAll()
     .then(mailforms => {
       res.json(mailforms);
+    })
+    .catch(err => {
+      next(err);
     });
-}
+};
 
-const searchByType = (req, res) => {
+const searchByType = (req, res, next) => {
   MailForm
     .scope({ method: ['whereType', req.params.type] }, 'passedFirst')
     .findAll()
     .then(mailforms => {
       res.json(mailforms);
+    })
+    .catch(err => {
+      next(err);
     });
-}
+};
 
 const show = (req, res) => {
   res.json(req.mailform);
-}
+};
 
-const store = (req, res) => {
-  MailForm.create(req.body)
+const store = (req, res, next) => {
+  MailForm
+    .create(req.body)
     .then(mailform => {
       res.status(201).json(mailform);
     })
     .catch(err => {
-      res.status(500).json(err);
+      next(err);
     });
-}
+};
 
-const update = (req, res) => {
-  let mailform = req.mailform;
-  for (let [key, value] of Object.entries(req.body)) {
-    mailform[key] = value;
-  }
-  mailform.save()
-    .then(updated => {
-      if (updated) {
-        res.status(204).end();
-      }
-      else {
-        throw new Error();
-      }
+const update = (req, res, next) => {
+  const previous = {
+    header_image: req.mailform.header_image,
+    map_image: req.mailform.map_image
+  };
+  req.mailform
+    .update(req.body)
+    .then(mailform => {
+      res.json(mailform);
+      ['header_image', 'map_image'].forEach(key => {
+        if (req.body[key] !== undefined) {
+          unlink(previous[key], () => console.log('successfully deleted ' + previous[key]));
+        }
+      });
     })
     .catch(err => {
-      res.status(500).end();
+      next(err);
     });
-}
+};
 
-const destroy = (req, res) => {
-  req.mailform.destroy()
-    .then(deleted => {
-      if (deleted) {
-        res.status(204).end();
-      }
-      else {
-        throw new Error();
-      }
+const destroy = (req, res, next) => {
+  req.mailform
+    .destroy()
+    .then(mailform => {
+      res.sendStatus(204);
+      ['header_image', 'map_image'].forEach(key => {
+        unlink(mailform[key], () => console.log('successfully deleted ' + mailform[key]));
+      });
     })
     .catch(err => {
-      res.status(500).end();
+      next(err);
     });
-}
+};
 
 module.exports = {
   index: index,
