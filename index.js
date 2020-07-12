c44e8d60fca17e7760de0abf69238d62f114134a
JongminChoe <aaaa961224@gmail.com>
2020-07-12 23:44:50 +0900

    전역 예외 처리기 추가
    에러 발생시 업로드된 파일 삭제

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const path = require('path');
 dotenv.config();
 const app = express();
+const { unlink } = require('fs');
 const cors = require("cors");
 const redis = require('redis')
 const redisClient = redis.createClient()
@@ -27,6 +28,24 @@ app.use(
 app.set("view engine", "ejs"); // .env를 사용하기 위한 ejs
 
 app.use(require('./routes'));
+app.use((err, req, res, next) => {
+  if (typeof req.files === 'object') {
+    Object.values(req.files).flat().forEach(file => {
+      unlink(file.path, err => {
+        if (err) {
+          return next(err);
+        }
+        console.log('successfully deleted ' + file.path);
+      });
+    });
+  }
+  next(err);
+});
+app.use((err, req, res, next) => {
+  if (!res.headersSent) {
+    res.sendStatus(500);
+  }
+});
 
 const server = app.listen(9170, () => console.log("서버 진행중 9170"));
 const io = require('socket.io')(server);
