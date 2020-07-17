6af62c1a5abc22e9d4f412f234341ebfe052d912
scd7896 <scd1212@naver.com>
2020-07-17 15:01:46 +0900

    파일 업로드시 생기는 문제

--- controller/mailform.js
+++ controller/mailform.js
@@ -60,7 +60,7 @@ const update = (req, res, next) => {
           try {
             unlink("public/" + previous[key], () => console.log("successfully deleted " + previous[key]));
           } catch (err) {
-            console.log("tet");
+            console.log("기존에 파일이 없습니다");
           }
         }
       });
