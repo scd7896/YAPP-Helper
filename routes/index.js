198e841dd06299a771de50136417545e2d49ec7e
scd7896 <scd1212@naver.com>
2020-08-13 23:59:45 +0900

    로그인 관련 컨트롤러 라우터 설계

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,21 @@
-const express = require('express');
+const express = require("express");
+const authMiddleWare = require("../middleware/auth");
 const router = express.Router();
 
 const apiRouter = express.Router();
 
-apiRouter.use('/mailforms', require('./mailform'));
-apiRouter.use('/email', require('./email'));
-apiRouter.use('/recruit', require('./recruit'));
+apiRouter.use("/mailforms", authMiddleWare, require("./mailform"));
+apiRouter.use("/email", authMiddleWare, require("./email"));
+apiRouter.use("/recruit", authMiddleWare, require("./recruit"));
+apiRouter.use("/login", authMiddleWare, require("./login"));
 // apiRouter.use('/file', require('../controller/file'));
-// apiRouter.use('/login', loginRouter);
-apiRouter.use('*', (req, res) => res.sendStatus(404));
+apiRouter.use("*", (req, res) => res.sendStatus(404));
 
-router.use('/api', apiRouter);
+router.use("/api", apiRouter);
 
-router.use(express.static('public'));
-router.use('/dist', express.static('dist')); // react 쓰기위한 것
-router.get('*', (req, res) => res.render('index'));
+router.use(express.static("public"));
+router.use("/dist", express.static("dist")); // react 쓰기위한 것
+router.get("/", (req, res) => res.render("index"));
 
+router.get("*", authMiddleWare, (req, res) => res.render("index"));
 module.exports = router;
