493b09867f4ce5cddc9e482de497b0400edc725d
scd7896 <scd1212@naver.com>
2020-08-13 23:59:24 +0900

    유저 모델 설계 및, 필요한 유저만 시드로 넣기

---
+++ seeders/20200809153042-users.js
@@ -0,0 +1,28 @@
+"use strict";
+const crypto = require("crypto");
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          token: crypto.createHash("sha512").update(process.env.GOOGLE_ACCESS.toString()).digest("base64"),
+          name: "supportYapp",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  },
+};
