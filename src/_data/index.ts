9a3e791c09b9e1fa09bf10581acea85fdecb6648
scd7896 <scd1212@naver.com>
2021-06-01 23:50:03 +0900

    개발 서버에서도 s3로 업로드할 수 있게함

--- src/_data/index.ts
+++ src/_data/index.ts
@@ -1,2 +1,1 @@
-export const url =
-  process.env.NODE_ENV === "production" && false ? "http://helper.yapp.co.kr" : "http://localhost:9170";
+export const url = process.env.NODE_ENV === "production" ? "http://helper.yapp.co.kr" : "http://localhost:9170";
